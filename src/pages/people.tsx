7af3702c81b1118674827165bbdda2debe737841
trang-1054 <51772237+trangtt-1054@users.noreply.github.com>
2020-07-11 22:06:49 +0700

    Update people.tsx

--- src/pages/people.tsx
+++ src/pages/people.tsx
@@ -5,7 +5,7 @@ export default function People({ people }: any) {
   return <h1>Hi, folks {JSON.stringify(people)}</h1>;
 }
 
-People.getInitialProps = async (ctx: NextPageContext) => {
+People.getInitialProps = async (ctx: NextPageContext) => { //this code run both on your server and everytime you navigate to this page on client side
   const json = await myGet("http://localhost:3000/api/people", ctx);
   return { people: json };
 };
